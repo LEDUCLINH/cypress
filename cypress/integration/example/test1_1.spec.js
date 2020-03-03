345a75a02b4cb3f35783203a0854ba9982870ccd
LuuVinhHung <luuvinhhung159@gmail.com>
2020-03-03 23:04:22 +0700

    Update fixtures users

---
+++ cypress/integration/example/test1_1.spec.js
@@ -0,0 +1,20 @@
+describe("Test login page", () => {
+  it("Test 1_1", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.fixture("users/superadmin.json").then(user => {
+      cy.get("input#normal_login_username")
+        .should("be.visible")
+        .clear()
+        .type(user.username);
+      cy.get("input#normal_login_password")
+        .should("be.visible")
+        .clear()
+        .type(user.password);
+      cy.get(".innos-ui-button")
+        .should("be.visible")
+        .click();
+      cy.wait(1000);
+    });
+  });
+});
